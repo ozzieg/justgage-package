2bb28e64de9001232feee49221513e15eb5e0e92
Ozzie <ozzie@gurkans.com>
2014-11-05 07:34:23 +0000

    added namespace to package

--- package.js
+++ package.js
@@ -1,5 +1,8 @@
 Package.describe({
-  summary: "A handy JavaScript plugin for generating and animating nice & clean dashboard gauge"
+  summary: "A handy JavaScript plugin for generating and animating nice & clean dashboard gauge",
+  name: "ozzieg:justgage-package",
+  version: "1.0.0",
+  git: "https://github.com/ozzieg/justgage-package.git"
 });
 
 Package.on_use(function (api) {
