258af095ad32078de6ac067c9c87b69b88823968
Ozzie <ozzie@gurkans.com>
2014-11-05 07:15:02 +0000

    updated to work with meteor 1.0 and clubfest:raphael

--- package.js
+++ package.js
@@ -2,10 +2,9 @@ Package.describe({
   summary: "A handy JavaScript plugin for generating and animating nice & clean dashboard gauge"
 });
 
-Package.on_use(function (api, where) {
-  api.use(['jquery'], 'client');
-  api.add_files(['lib/justGage/justgage.js'], 'client');
-  if (typeof api.export !== 'undefined') {
-		api.export("JustGage", "client");
-	}
+Package.on_use(function (api) {
+  //api.use('jquery', 'client');
+  api.use('clubfest:raphael');
+  api.add_files('justgage.1.0.1.min.js', 'client');
+  api.export('JustGage', 'client');
 });
